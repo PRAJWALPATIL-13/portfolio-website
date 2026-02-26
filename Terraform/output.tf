d8e31207aeff406b0a38b5943a7fc91c66ef2770
PrajwalPatil-13 <prajwalpatil128@gmail.com>
2026-02-26 16:07:51 +0530

    Initial commit: Portfolio website and Terraform configs

---
+++ Terraform/output.tf
@@ -0,0 +1,16 @@
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.portfolio.website_endpoint
+}
+
+output "s3_bucket_name" {
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.portfolio.id
+}
+
+output "upload_command" {
+  description = "Command to sync files"
+  value       = <<-EOT
+    aws s3 sync "C:\Users\Prajwal\.gemini\antigravity\scratch\devops-portfolio" s3://prajwal-portfolio-bucket2026 --exclude 'terraform/*' --exclude '.git/*'
+  EOT
+}
